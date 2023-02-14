efb361fbe1ecaa25267660deb7dfffd4920e366e
Konstantin Kochnev <kostyawhite89@gmail.com>
2023-02-14 20:56:14 +0300

    Run tests via docker-compose

--- src/tests/functional/src/test_signup.py
+++ src/tests/functional/src/test_signup.py
@@ -5,7 +5,7 @@ import requests
 
 @pytest.mark.asyncio
 async def test_first_signup(make_post_request):
-    data = json.dumps({"email": "email", "password": "password"}).encode()
+    data = json.dumps({"email": "email@test.ru", "password": "password"}).encode()
     response = await make_post_request(url=f'/api/v1/user/signup', data=data)
     assert response.status == HTTPStatus.OK
 
