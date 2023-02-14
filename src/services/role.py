8188e8d89e5d454c3ca77cc47ab9176f9960d70f
Arthur Lakiza <lakiza@gmail.com>
2023-02-14 03:02:06 +0300

    Добавлены ручки для ролей

--- src/services/role.py
+++ src/services/role.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from uuid import UUID
 from typing import Type
 
 from flask_sqlalchemy import SQLAlchemy
@@ -12,16 +13,43 @@ class RoleService:
         self.model_role = model_role
         self.model_role_schema = model_role_schema
 
-    def get_by_name(self, name: str) -> Role | None:
+    def _get_by_name(self, name: str) -> Role | None:
+        """
+        Поиск роли по наименованию.
+
+        :param name: Название роли
+        :return: None - если указанной роли не существует
+                 или объект Role роли
+        """
         return self.model_role.query.filter_by(name=name).one_or_none()
 
-    def get_all(self):
+    def _get_by_id(self, role_id: UUID) -> Role | None:
+        """
+        Поиск роли по индификатору.
+
+        :param role_id: Индификатор роли
+        :return: None - если указанной роли не существует
+                 или объект Role роли
+        """
+        return self.model_role.query.filter_by(id=role_id).one_or_none()
+
+    def get_all(self) -> list[RoleSchema]:
+        """
+        Возвращает список ролей.
+        """
         roles = self.model_role.query.all()
         return self.model_role_schema(many=True).dump(roles)
 
-    def create(self, name: str, description: str) -> RoleSchema | None:
+    def create(self, name: str, description: str = '') -> RoleSchema | None:
+        """
+        Создает новую роль.
 
-        if self.get_by_name(name):
+        :param name: Название новой роли
+        :param description: Описание новой роли
+        :return: None - если указанной роли не существует
+                 или объект RoleSchema новой роли
+        """
+        if self._get_by_name(name):
             return None
 
         role = self.model_role(name=name, description=description)
@@ -29,6 +57,49 @@ class RoleService:
         self.db.session.commit()
 
         return self.model_role_schema().dump(role)
+
+    def delete(self, role_id: UUID) -> bool | None:
+        """
+        Удаляет существующую роль.
+
+        :param role_id: Индификатор роли
+        :return: None - если указанной роли не существует
+                 True - в случае успеха
+        """
+        role = self._get_by_id(role_id)
+        if not role:
+            return None
+
+        self.db.session.delete(role)
+        self.db.session.commit()
+
+        return True
+
+    def update(self, role_id: UUID, payload: dict) -> bool | None:
+        """
+        Обновляет существующую роль.
+
+        :param role_id: Индификатор роли
+        :param payload: Словарь с полями для обновления
+        :return: None - если указанной роли не существует
+                 False - если меняем название роли на уже существующее
+                 True - в случае успеха
+        """
+        role = self._get_by_id(role_id)
+        if not role:
+            return None
+
+        if 'name' in payload:
+            other_role = self._get_by_name(payload['name'])
+            if other_role and other_role.id != role.id:
+                return False
+
+        for key, value in payload.items():
+            setattr(role, key, value)
+
+        self.db.session.commit()
+
+        return True
 
 
 @lru_cache()
