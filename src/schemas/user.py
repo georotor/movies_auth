b20618d74aae74fd620da66d3036c7fdecca25f1
Pavel Krizhepolskiy <re-times@yandex.ru>
2023-02-19 03:47:27 +0300

    Добавил схемы UserSchema и UserHistorySchema

---
+++ src/schemas/user.py
@@ -0,0 +1,23 @@
+from marshmallow import post_load
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from models.user import User, UserHistory
+
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        load_only = ("password",)
+        include_relationships = True
+        fields = ('email', 'password', 'is_admin')
+
+
+class UserHistorySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserHistory
+        fields = ('user_id', 'user_agent', 'action')
+
+    @post_load
+    def create_user_history(self, data, **kwargs):
+        return UserHistory(**data)
