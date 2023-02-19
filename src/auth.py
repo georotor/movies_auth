09f7538355674727a76e638cee6e5446eeaa5922
Pavel Krizhepolskiy <re-times@yandex.ru>
2023-02-19 03:53:44 +0300

    Убрал лишнюю логику из ручек, подключил services

--- src/auth.py
+++ src/auth.py
@@ -1,109 +1,103 @@
-"""
-Вопрос номер один - где хранятся активные токены? Редис? Куки? Psql?
-Вопрос номер два - как должен выглядеть класс юзера из сервисов?
-
-"""
-
-
 from http import HTTPStatus
 
-from datetime import datetime, timedelta
+from flask import Blueprint, jsonify, make_response, request
+from flask_jwt_extended import jwt_required
 
-from flask import Blueprint, request, jsonify, make_response
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-    jwt_required,
-    get_jwt,
-    get_jti,
-    get_jwt_identity,
-    JWTManager,
-)
-
-from services.user import UserService
+from services.auth import AuthError, AuthService
+from services.token import TokenService
 
 auth = Blueprint("auth", __name__)
-jwt = JWTManager()
 
-# sess = scoped_session(sessionmaker())  # UserSchema().load(jsn, session=sess)
+auth_service = AuthService()
+token_service = TokenService()
 
 
 @auth.route('/register', methods=['POST'])
 def create_new_user():
-    username = request.values.get('username', None)
+    """Регистрация: создание нового пользователя и возврат пары токенов.
+    Данные можно или получать в json или брать из заголовка http request.
+
+    На данный момент используется request.json, но можно легко переделать:
+
+    email = request.values.get('email', None)
     password = request.values.get('password', None)
 
-    user_host = request.remote_addr
-    user_agent = request.user_agent
+    """
+    try:
+        user_id = auth_service.registration(request.json)
+    except AuthError as e:
+        return jsonify({'msg': str(e)}), HTTPStatus.CONFLICT
 
-    if not username or not password:
-        msg = jsonify({'msg': 'Missing mandatory parameters.'})
-        return msg, HTTPStatus.BAD_REQUEST
+    user_host = request.remote_addr  # todo: добавить в UserHistory
+    user_agent = request.user_agent.string
+    auth_service.remember_login(user_id, user_agent, action='registration')
 
-    user = UserService()
-    user.create(username, password)
-
-    if not user.id:  # todo: костыль, нужно менять метод в UserService
-        msg = jsonify({'msg': 'User already exists.'})
-        return msg, HTTPStatus.CONFLICT
-
-    user.log(user_host, user_agent)
-
-    access_token = create_access_token(identity=user.id, fresh=True)
-    refresh_token = create_refresh_token(identity=user.id)
-
-    msg = jsonify(
-        {'msg': 'User successfully created.',
-         'access_token': access_token,
-         'refresh_token': refresh_token}
+    access_token, refresh_token = token_service.create(
+        user_id, user_agent, fresh=True
     )
-    return make_response(msg, HTTPStatus.CREATED)
+
+    return jsonify(
+        access_token=access_token, refresh_token=refresh_token
+    ), HTTPStatus.CREATED
 
 
 @auth.route('/login', methods=['POST'])
 def login():
-    username = request.authorization.username
-    password = request.authorization.password
 
-    if not username or not password:
-        msg = jsonify({'msg': 'Authentication faild.'})
-        return make_response(msg, HTTPStatus.UNAUTHORIZED)
+    user_id = auth_service.login(request.json)
 
-    user = UserService()
-    if user.login(username, password) is None:
-        msg = jsonify({'msg': 'Authentication faild.'})
-        return make_response(msg, HTTPStatus.UNAUTHORIZED)
+    user_host = request.remote_addr  # todo: добавить в UserHistory
+    user_agent = request.user_agent.string
+    auth_service.remember_login(user_id, user_agent)
 
-    access_token = create_access_token(identity=user.id, fresh=True)
-    refresh_token = create_refresh_token(identity=user.id)
-    # ключ для редиса - jti, он же unique identifier of an encoded JWT
-    redis_key = get_jti(refresh_token)  # todo: добавить запись в Redis
+    access_token, refresh_token = token_service.create(user_id, user_agent)
 
-    msg = jsonify(
-        {'msg': 'Authentication successfully completed.',
-         'access_token': access_token,
-         'refresh_token': refresh_token}
-    )
-    return make_response(msg, HTTPStatus.OK)
+    return jsonify(
+        access_token=access_token, refresh_token=refresh_token
+    ), HTTPStatus.OK
 
 
 @auth.route("/logout", methods=["DELETE"])
-@jwt_required()
+@jwt_required(verify_type=False)
 def logout():
-    # https://flask-jwt-extended.readthedocs.io/en/stable/blocklist_and_token_revoking/
-    # ключ для редиса - jti, он же unique identifier of an encoded JWT
-    redis_key = get_jwt()["jti"]  # todo: удалить запись из Redis
-    msg = jsonify({'msg': 'Logout successfully completed.'})
-    return make_response(msg, HTTPStatus.OK)
+    """Есть несколько разных способов отзывать токены при выходе пользователя.
+    В документации https://flask-jwt-extended.readthedocs.io/en/stable/blocklist_and_token_revoking/#redis
+    сказано, что ответственность за генерацию запросов на отзыв и access и
+    refresh токенов обычно ложится на frontend. Со стороны backend достаточно
+    предоставить один универсальный метод @jwt_required(verify_type=False), с
+    помощью которого можно отозвать любой токен.
+
+    Важный момент! Отзывать только access токен бессмысленно - пока "жив"
+    refresh токен пользователь сможет запросить новый.
+
+    """
+    user_agent = request.headers['user_agent']
+    ex_token_type = token_service.delete(user_agent)
+    return jsonify(msg="{} token successfully revoked".format(ex_token_type))
 
 
 @auth.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
-    # https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
-    exp_timestamp = get_jwt()["exp"]
-    now = datetime.now()
-    target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-    if target_timestamp > exp_timestamp:
-        access_token = create_access_token(identity=get_jwt_identity())
+    """Получение новой пары токенов на основании действующего refresh токена.
+    Перед этим убеждаемся, что refresh токен и user_agent не менялись.
 
+    """
+    user_agent = request.headers['user_agent']
+
+    if not token_service.check_owner(user_agent):
+        return jsonify(
+            msg='Token does not belong to this user'
+        ), HTTPStatus.CONFLICT
+
+    access_token, refresh_token = token_service.refresh(user_agent)
+
+    return jsonify(
+        access_token=access_token, refresh_token=refresh_token
+    ), HTTPStatus.OK
+
+
+@auth.route("/protected")
+@jwt_required()
+def protected():
+    return make_response({'msg': 'OK'}, HTTPStatus.OK)
