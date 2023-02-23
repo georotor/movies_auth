8d55a9198abe8566f27a01127bcaf34d5963f8e7
Pavel Krizhepolskiy <re-times@yandex.ru>
2023-02-23 13:31:01 +0300

    Перенес схемы из auth в user

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,4 +1,4 @@
-from marshmallow import post_load
+from marshmallow import post_load, Schema, fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from models.user import User, UserHistory
@@ -22,3 +22,57 @@ class UserHistorySchema(SQLAlchemyAutoSchema):
     @post_load
     def create_user_history(self, data, **kwargs):
         return UserHistory(**data)
+
+
+class LoginSchema(Schema):
+    """Схема для аутентификации пользователя. Возвращает модель пользователя
+    либо поднимает ошибку.
+
+    """
+    email = fields.String(required=True)
+    password = fields.String(required=True)
+
+    @post_load
+    def create_user(self, data, **kwargs):
+        return User(**data)
+
+
+class RegistrationSchema(Schema):
+    """Схема для регистрации пользователя. Возвращает модель пользователя.
+
+    Валидация полей отдана restx, данные проверяются на уровне api.
+    При необходимости ее можно реализовать в схеме вот так:
+
+    @validates("password")
+    def validates_password(self, value):
+        if len(value) < 8:
+            raise ValidationError("Password must be at least 8 characters")
+
+    """
+    email = fields.String(required=True)
+    password = fields.String(required=True)
+
+    @post_load
+    def create_user(self, data, **kwargs):
+        return User(**data)
+
+
+class UpdateUserSchema(Schema):
+    """Схема для обновления данных. Возвращает модель пользователя.
+
+    Валидация полей отдана restx, данные проверяются на уровне api.
+    При необходимости ее можно реализовать в схеме вот так:
+
+    @validates("password")
+    def validates_password(self, value):
+        if len(value) < 8:
+            raise ValidationError("Password must be at least 8 characters")
+
+    """
+    id = fields.UUID(required=True)
+    email = fields.String(required=False)
+    password = fields.String(required=False)
+
+    @post_load
+    def create_user(self, data, **kwargs):
+        return User(**data)
